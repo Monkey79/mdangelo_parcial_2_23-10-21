9c4becf635204a19013c878cc2095a56df2fdad6
Monkey79 <marianomartin79@gmail.com>
2021-11-20 12:18:46 -0300

    parcial_2 init project

---
+++ src/mdangelo_parc_2_231021.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : mdangelo_parc_2_231021.c
+ Author      : mdangelo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!parcial laboratorio 23-nov-2021 (divison h)!!!"); /* prints !!!parcial laboratorio 23-nov-2021 (divison h)!!! */
+	return EXIT_SUCCESS;
+}
